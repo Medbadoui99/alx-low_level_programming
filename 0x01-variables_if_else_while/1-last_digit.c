9fe15b966195ccd1de769c4d1b254ebc85b9f73b
Medbadoui99 <mohamedsi692@gmail.com>
2023-03-17 17:36:15 -0700

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@
  */
 int main(void)
 {
-     int n;
+int n;
 
-     srand(time(0));
-       n = rand() - RAND_MAX / 2;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 5)
 	{
@@ -35,5 +35,6 @@ int main(void)
 			n, n % 10);
 	}
 
-       return (0);
+return (0);
+
 }
