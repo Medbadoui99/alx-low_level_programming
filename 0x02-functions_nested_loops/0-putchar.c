349a8774fc7db614148b903c4dd687330156cf07
Medbadoui99 <mohamedsi692@gmail.com>
2023-03-21 15:33:41 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,8 +11,15 @@
  */
 int main(void)
 {
-	putchar("_putchar");
-	putchar("\n");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 
 	return (0);
 }
