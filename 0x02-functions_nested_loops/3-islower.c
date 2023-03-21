f2e0e683dcd61752cad84ed31c83c01362267911
Medbadoui99 <mohamedsi692@gmail.com>
2023-03-21 16:48:11 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+/*
+ * File: 3-islower.c
+ */
+
+#include "main.h"
+
+/**
+ * _islower - function that checks for lowercase character.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
