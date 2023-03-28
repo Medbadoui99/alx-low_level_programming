3967802c677fc377887ef5bed3345278c92f4231
Medbadoui99 <mohamedsi692@gmail.com>
2023-03-28 14:07:40 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,10 @@
  */
 void print_rev(char *s)
 {
-	int len = strlen(s), i;
-	
-	while (s[i++])
-		len++;
+	int l = strlen(s), i;
 
-	for (i = len - 1; i >= 0; i--)
-		_putchar(s[i]
+	for (i = l - 1; i >= 0; i--)
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
