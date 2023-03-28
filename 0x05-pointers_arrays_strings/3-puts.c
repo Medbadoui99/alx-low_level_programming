94c49d8cdf49df012b55430d96581892deff9536
Medbadoui99 <mohamedsi692@gmail.com>
2023-03-27 19:14:41 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+/*
+ * File: 3-puts.c
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string,followed by a new line,
+ *         to stdout.
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
