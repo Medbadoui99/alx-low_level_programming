96fd74268a37e4cf6262bc68c666d030aff5928d
Medbadoui99 <mohamedsi692@gmail.com>
2023-03-28 17:05:41 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,8 +12,18 @@ void print_rev(char *s)
 {
 	int len = 0, index;
 
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	s--;
+
 	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	{
+		_putchar(*s);
+		s--;
+	}
 
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -18,8 +18,10 @@ char *_strcpy(char *dest, char *src)
 	int index = 0;
 
 	while (src[index])
+	{
 		dest[index] = src[index];
 		index++;
+	}
 
 	return (dest);
 }
