8545cadb8ec366e8ea45c2cfe4a309fa1d1bd96b
Medbadoui99 <mohamedsi692@gmail.com>
2023-03-31 15:11:50 -0700

    More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -21,7 +21,7 @@ char *leet(char *str)
 		{
 			if (str[index1] == leet[index2] ||
 			    str[index1] - 32 == leet[index2])
-				str[index] - index2 + '0';
+				str[index] = index2 + '0';
 		}
 
 		index1++;
